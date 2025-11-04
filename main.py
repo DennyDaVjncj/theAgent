4dd69bf85672068eafa1c93a70858ce9e8f6fe89
DennyDaVjncj <daVjncjCode@gmail.com>
2025-11-04 14:17:36 -0500

    tentative update to main file

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Welcome to le da Vjncj code")
+# def main():
+#     print("Welcome to le da Vjncj code")
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key=os.environ.get("GEMINI_API_KEY")
