6ec34d74c23f4ca4bfff7c6bcacb572d079db8de
DennyDaVjncj <daVjncjCode@gmail.com>
2025-11-09 12:51:52 -0500

    syntax correction

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,7 +2,7 @@ import os
 
 def get_files_info(working_directory,directory=None):
     abs_working_dir=os.path.abspath(working_directory)
-    if directory is none:
+    if directory is None:
         directory= "."
     abs_working_dir= os.path.abspath(directory)
     if not abs_working_dir.startswith(abs_working_dir):
