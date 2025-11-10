988721b441399556e4a6ab8381b2e5c633768248
DennyDaVjncj <daVjncjCode@gmail.com>
2025-11-10 08:53:59 -0500

    agent FS traversal configured

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -6,4 +6,13 @@ def get_files_info(working_directory,directory=None):
         directory= "."
     abs_working_dir= os.path.abspath(directory)
     if not abs_working_dir.startswith(abs_working_dir):
+        return f'Error: "{directory}" is not a working directory.'
+    
+    final_response=""
+    contents=os.listdir(abs_working_dir)
+    for content in contents:
+        is_dir=os.path.isdir(os.path.join(abs_working_dir,content))
+        size=os.path.getsize(content_path)
+        final_response+=f"-{content}: file_size={size}bytes,is_dir={is_dir}/n"
+    return final_response 
         
