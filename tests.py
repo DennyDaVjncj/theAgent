7d1d542d0318a04371512c025ed77c73a93d94e6
DennyDaVjncj <daVjncjCode@gmail.com>
2025-11-17 12:20:33 -0500

    root tests file updated

--- tests.py
+++ tests.py
@@ -1,10 +1,10 @@
 # from DIRNAME.FILENAME import FUNCTION_NAME
 from functions.get_files_info import get_files_info
+# from os
 
 def main():
     get_files_info("calculator",".")
     get_files_info("calculator","pkg")
     get_files_info("calculator","/bin")
     get_files_info("calculator","../")
-
-
+    # get_files_info(working_dir)
