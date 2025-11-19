fd8c1762dac521734654912fa8abef64386a0157
DennyDaVjncj <daVjncjCode@gmail.com>
2025-11-19 09:13:51 -0500

    tentative updates to root tests.py

--- tests.py
+++ tests.py
@@ -3,8 +3,14 @@ from functions.get_files_info import get_files_info
 # from os
 
 def main():
-    get_files_info("calculator",".")
-    get_files_info("calculator","pkg")
-    get_files_info("calculator","/bin")
-    get_files_info("calculator","../")
+    working_dir="claculator"
+    root_contents=get_files_info(working_dir)
+    print(root_contents)
+    
+# def main():
+#     get_files_info("calculator",".")
+#     get_files_info("calculator","pkg")
+#     get_files_info("calculator","/bin")
+#     get_files_info("calculator","../")
     # get_files_info(working_dir)
+
