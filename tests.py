4a7f428be6453c08426050bed6bf58b6a5bb6f83
DennyDaVjncj <daVjncjCode@gmail.com>
2025-11-20 12:18:14 -0500

    tests.py optimized

--- tests.py
+++ tests.py
@@ -1,11 +1,19 @@
 # from DIRNAME.FILENAME import FUNCTION_NAME
 from functions.get_files_info import get_files_info
-# from os
 
 def main():
     working_dir="claculator"
     root_contents=get_files_info(working_dir)
     print(root_contents)
+    pkg_contents=get_files_info(working_dir,"pkg")
+    print(pkg_contents)
+    pkg_contents=get_files_info(working_dir,"/bin")
+    print(pkg_contents)
+    pkg_contents=get_files_info(working_dir,"../")
+    print(pkg_contents)
+
+    main()
+    
     
 # def main():
 #     get_files_info("calculator",".")
