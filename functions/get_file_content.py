81de491b7a695117c5c9556a7e88f0825e8dc69c
DennyDaVjncj <daVjncjCode@gmail.com>
2025-11-24 13:29:54 -0500

    os package imported

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,6 +1,10 @@
+import os
+
 def get_file_content(working_directory,file_path):
     abs_working_dir=os.path.abspath(working_directory)
     abs_working_dir=os.path.abspath(file_path)
+
+
 
 """ *Err strings with good context, is key for developing effective agents.
 *We ended with developing the agent's ability to visit files and read their contents.
