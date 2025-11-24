4558140ccef9b5276e0c78c4465898794ca79eae
DennyDaVjncj <daVjncjCode@gmail.com>
2025-11-24 14:56:17 -0500

    agent's ability to retrieve file info under construction

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,9 +1,16 @@
 import os
+MAX_CHARS=10000#A stant base value. 
 
 def get_file_content(working_directory,file_path):
     abs_working_dir=os.path.abspath(working_directory)
-    abs_working_dir=os.path.abspath(file_path)
+    abs_file_path=os.path.abspath(os.path.join(working_directory,file_path))#updated path, to "abs_file".
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_file_path):
+        return f'Error:"{file_path}" is not a file.'
 
+f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+f'Error: File not found or is not a regular file: "{file_path}"'
 
 
 """ *Err strings with good context, is key for developing effective agents.
