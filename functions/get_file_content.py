7a09968799922e8c048018c4f2374e132c85be5c
DennyDaVjncj <daVjncjCode@gmail.com>
2025-11-24 13:27:16 -0500

    note(s) to self

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -5,4 +5,10 @@ def get_file_content(working_directory,file_path):
 """ *Err strings with good context, is key for developing effective agents.
 *We ended with developing the agent's ability to visit files and read their contents.
 
+F.S. FUNCTION(S):
+*os.path.abspath: Get an absolute path from a relative path
+os.path.join: Join two paths together safely (handles slashes)
+.startswith: Check if a string starts with a specific substring
+os.path.isfile: Check if a path is a file
+
 """
