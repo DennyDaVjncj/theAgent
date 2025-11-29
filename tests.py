fcedc7103dff5e1e106a47146ed19abe6e409a2d
DennyDaVjncj <daVjncjCode@gmail.com>
2025-11-29 12:12:23 -0500

    upgraded root testing

--- tests.py
+++ tests.py
@@ -3,15 +3,17 @@ from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 
 def main():
-    working_dir="claculator"
-    root_contents=get_files_info(working_dir)
-    print(root_contents)
-    pkg_contents=get_files_info(working_dir,"pkg")
-    print(pkg_contents)
-    pkg_contents=get_files_info(working_dir,"/bin")
-    print(pkg_contents)
-    pkg_contents=get_files_info(working_dir,"../")
-    print(pkg_contents)
+    # working_dir="claculator"
+    # root_contents=get_files_info(working_dir)
+    # print(root_contents)
+    # pkg_contents=get_files_info(working_dir,"pkg")
+    # print(pkg_contents)
+    # pkg_contents=get_files_info(working_dir,"/bin")
+    # print(pkg_contents)
+    # pkg_contents=get_files_info(working_dir,"../")
+    # print(pkg_contents)
+    working_dir="calculator"
+    print(get_file_content(working_dir,"lorem.txt"))
 
     main()
     
