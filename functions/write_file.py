f4c810182435e056528f1c9c6b9c4febe8a072cd
DennyDaVjncj <daVjncjCode@gmail.com>
2025-12-01 09:52:23 -0500

    troubshoot update

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,4 +1,4 @@
 import os
 
 def write_file(working_directory, file_path, content):
-    f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+   return print(f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory')
