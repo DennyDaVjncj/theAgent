cdb4b64dccb3691df169313acef21eb86319ea78
DennyDaVjncj <daVjncjCode@gmail.com>
2025-12-16 09:03:30 -0500

    absFilePath provides dynamic endpoint values.

--- functions/write_file.py
+++ functions/write_file.py
@@ -3,6 +3,8 @@ import os
 
 def write_file(working_directory, file_path, content):
    abs_working_dir=os.path.abspath(working_directory)
+
+   #This variable provides a dynamic value for every location in-memory it's used. Same path to root, new endpoint.
    abs_file_path=os.path.abspath(os.path.join(working_directory,file_path))
    
    if not abs_file_path.startswith(abs_working_dir):
