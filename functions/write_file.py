8ce192ed3904341c72069cd14730754574f5ca91
DennyDaVjncj <daVjncjCode@gmail.com>
2025-12-21 13:20:17 -0500

    mini update to try/catch block

--- functions/write_file.py
+++ functions/write_file.py
@@ -4,13 +4,13 @@ import os
 def write_file(working_directory, file_path, content):
    abs_working_dir=os.path.abspath(working_directory)
 
-   #This variable provides a dynamic value for every location in-memory it's used. Same path to root, new endpoint.
+   #This variable provides a dynamic value for every location in-memory, it's used. Same path to root, new endpoint.
    abs_file_path=os.path.abspath(os.path.join(working_directory,file_path))
    
    if not abs_file_path.startswith(abs_working_dir):
       return f'Error: Cannot write to "{file_path}" as it is not in the working directory'
    if not os.path.isfile(abs_file_path):
-      parent_dir=os.path.dirname(abs_file_path)
+      parent_dir=os.path.dirname(abs_file_path)#updated endpoint
       try:
          os.makedirs(parent_dir)
       except Exception as e:
@@ -19,6 +19,7 @@ def write_file(working_directory, file_path, content):
       with open(abs_file_path, "w") as f:
          f.write(content)
       return f'Successfully wrote to "{file_path}" .' 
+   except Exception as e:
    
 """
 * Open(x,'w'), 2nd parameter indicates write mode.
