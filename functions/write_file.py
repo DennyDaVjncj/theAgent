ce7e7fa907c51572385b13d0cc7a759d6322dfc4
DennyDaVjncj <daVjncjCode@gmail.com>
2025-12-31 10:22:59 -0500

    optimized try/catch block

--- functions/write_file.py
+++ functions/write_file.py
@@ -2,8 +2,8 @@ import os
 #abs_file_path=os.path.abspath(os.path.join(working_directory,file_path))
 
 def write_file(working_directory, file_path, content):
-   abs_working_dir=os.path.abspath(working_directory)
-   abs_file_path=os.path.abspath(os.path.join(working_directory,file_path))
+   abs_working_dir=os.path.abspath(working_directory)#based on top level of project
+   abs_file_path=os.path.abspath(os.path.join(working_directory,file_path))  
    
    if not abs_file_path.startswith(abs_working_dir):
       return f'Error: Cannot write to "{file_path}" as it is not in the working directory'
@@ -11,13 +11,14 @@ def write_file(working_directory, file_path, content):
       parent_dir=os.path.dirname(abs_file_path)
       try:
          os.makedirs(parent_dir)
-      except Exception as e:
+      except Exception as e: 
          return f'Could not create directories for "{parent_dir}": {e}'
    try:
       with open(abs_file_path, "w") as f:
          f.write(content)
       return f'Successfully wrote to "{file_path}" .' 
-   
+   except Exception as e:
+      return f'Error writing to "{file_path}": {e}'
 """
 * Open(x,'w'), 2nd parameter indicates write mode.
 * Command line, shell, terminal are the links between platforms!
